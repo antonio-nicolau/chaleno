697bfe7fe1dd242625c6556081e887dda02e2376
António Nicolau <antonio.tioypedro1234@gmail.com>
2021-03-21 15:19:54 +0100

    get any attr and remove flutter dependece

--- test/chaleno_test.dart
+++ test/chaleno_test.dart
@@ -9,22 +9,6 @@ void main() {
     parser = await Chaleno().load('https://github.com/antonio-nicolau');
   });
 
-  group('Test chaleno repository class', () {
-    test('Get internet url', () {
-      expect(parser?.html, isNotEmpty);
-    });
-
-    test('Get LocalHost with LocalHost name', () async {
-      parser = await Chaleno().load('http://localhost/index');
-      expect(parser?.html, isNotEmpty);
-    });
-
-    test('Get LocalHost with IP', () async {
-      parser = await Chaleno().load('http://192.168.1.122/index');
-      expect(parser?.html, isNotEmpty);
-    });
-  });
-
   group('Test Parser class', () {
     test('Extract data', () {
       expect(parser?.getElementsByClassName('repo').length, greaterThan(0));
